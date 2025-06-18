342f0c120faac2fd90629023c20537bfab29d398
daviderivolta-ett <davide.rivolta@dedagroup.it>
2025-06-18 16:01:44 +0200

    Station class

--- src/app/models/station/index.ts
+++ src/app/models/station/index.ts
@@ -1,3 +1,4 @@
 export * from './basin-data.interface'
-export * from './station-data.interface'
 export * from './station-base.class'
+export * from './station-data.interface'
+export * from './station.class'
