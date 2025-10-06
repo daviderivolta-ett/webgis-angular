883a0cbe79bf8abb487e17a7ab2e73c68c6cf6fc
daviderivolta-ett <davide.rivolta@dedagroup.it>
2025-10-06 14:55:46 +0200

    Floating dialog resize

--- src/app/components/floating-dialog/floating-dialog.component.ts
+++ src/app/components/floating-dialog/floating-dialog.component.ts
@@ -1,5 +1,5 @@
 /** Libraries */
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 
 /** Component */
@@ -13,5 +13,57 @@ import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
   styleUrl: './floating-dialog.component.scss'
 })
 export class FloatingDialogComponent {
+  private _isResizing: boolean = false;
+
+  private _initialWidth: number = 0;
+  private _initialHeight: number = 0;
+  private _startX: number = 0;
+  private _startY: number = 0;
+
+  @ViewChild('floatingDialog') dialog!: ElementRef<HTMLDivElement>;
+
   constructor() { }
+
+  public ngOnDestroy(): void {
+    this.onResizeEnd();
+  }
+
+  private _onMouseMove = this.onResize.bind(this);
+  private _onMouseUp = this.onResizeEnd.bind(this);
+
+  public onResizeStart(event: MouseEvent): void {
+    this._isResizing = true;
+
+    this._initialWidth = this.dialog.nativeElement.offsetWidth;
+    this._initialHeight = this.dialog.nativeElement.offsetHeight;
+    this._startX = event.clientX;
+    this._startY = event.clientY;
+
+    document.addEventListener('mousemove', this._onMouseMove);
+    document.addEventListener('mouseup', this._onMouseUp);
+  }
+
+  public onResize(event: MouseEvent): void {
+    if (!this._isResizing) return;
+
+    const deltaX = event.clientX - this._startX;
+    const deltaY = event.clientY - this._startY;
+
+    const maxWidth = window.innerWidth - this.dialog.nativeElement.offsetLeft;
+    const maxHeight = window.innerHeight - this.dialog.nativeElement.offsetTop;
+
+    const newWidth = Math.min(this._initialWidth + deltaX, maxWidth);
+    const newHeight = Math.min(this._initialHeight + deltaY, maxHeight);
+
+    this.dialog.nativeElement.style.width = `${newWidth}px`;
+    this.dialog.nativeElement.style.height = `${newHeight}px`;
+  }
+
+  public onResizeEnd(): void {
+    if (!this._isResizing) return;
+    this._isResizing = false;
+
+    document.removeEventListener('mousemove', this._onMouseMove);
+    document.removeEventListener('mouseup', this._onMouseUp);
+  }
 }
