2a9f3db574aa0ace39c174463f1006537d1d43ed
daviderivolta-ett <davide.rivolta@dedagroup.it>
2025-10-06 12:45:27 +0200

    Angular update and setting drag and drop cdk

--- src/app/components/floating-dialog/floating-dialog.component.ts
+++ src/app/components/floating-dialog/floating-dialog.component.ts
@@ -1,11 +1,17 @@
+/** Libraries */
 import { Component } from '@angular/core';
+import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 
+/** Component */
 @Component({
   selector: 'app-floating-dialog',
-  imports: [],
+  imports: [
+    CdkDrag,
+    CdkDragHandle
+  ],
   templateUrl: './floating-dialog.component.html',
   styleUrl: './floating-dialog.component.scss'
 })
 export class FloatingDialogComponent {
-
+  constructor() { }
 }
