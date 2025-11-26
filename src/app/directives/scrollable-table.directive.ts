c79dce7393533dfcd39f78d4fbd59aa1b718f9af
daviderivolta-ett <davide.rivolta@dedagroup.it>
2025-11-26 11:51:42 +0100

    New scrollable tables

--- src/app/directives/scrollable-table.directive.ts
+++ src/app/directives/scrollable-table.directive.ts
@@ -6,6 +6,7 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
   selector: '[scrollableTable]'
 })
 export class ScrollableTableDirective {
+  private _unlisteners: (() => void)[] = [];
 
   constructor(
     private elementRef: ElementRef<HTMLDivElement>,
@@ -14,38 +15,67 @@ export class ScrollableTableDirective {
 
   // Directive lifecycle
   public ngAfterViewChecked(): void {
-    const table: HTMLTableElement | null = this.elementRef.nativeElement.querySelector('table');
+    const table: HTMLDivElement | null = this.elementRef.nativeElement.querySelector('.table');
     if (table) {
-      this._setTableProperties(this.elementRef.nativeElement);
       this._searchHeaderCell(table);
+      const thead = this._searchTableHead(table);
+      const tbody = this._searchTableBody(table);
+      if (thead && tbody) {
+        this._setTableHeadProperties(thead);
+        this._syncScroll(thead, tbody);
+      }
     }
+  }
+
+  ngOnDestroy(): void {
+    this._unlisteners.forEach(unlisten => unlisten());
+    this._unlisteners = [];
   }
 
   // Methods
-  private _setTableProperties(container: HTMLDivElement): void {
-    this.renderer.setStyle(container, 'display', 'block');
-    this.renderer.setStyle(container, 'overflowX', 'auto');
+  private _setTableHeadProperties(thead: HTMLDivElement): void {
+    this.renderer.setStyle(thead, 'position', 'sticky');
+    this.renderer.setStyle(thead, 'top', '0');
   }
 
-  private _searchHeaderCell(table: HTMLTableElement): void {
-    const firstTh: Element | null = table.querySelector('thead tr th'); 
-    if (firstTh && firstTh instanceof HTMLTableCellElement) {
+  private _searchTableHead(table: HTMLDivElement): HTMLDivElement | null {
+    return table.querySelector('.thead');
+  }
+
+  private _searchTableBody(table: HTMLDivElement): HTMLDivElement | null {
+    return table.querySelector('.tbody');
+  }
+
+  private _searchHeaderCell(table: HTMLDivElement): void {
+    const firstTh: Element | null = table.querySelector('.thead .trow .tdata');
+    if (firstTh && firstTh instanceof HTMLDivElement) {
       this._makeSticky(firstTh, '0px');
     }
 
-    const bodyRows: NodeListOf<Element> = table.querySelectorAll('tbody tr');
+    const bodyRows: NodeListOf<Element> = table.querySelectorAll('.tbody .trow');
     bodyRows.forEach((row: Element, index: number) => {
-      const firstTd: HTMLTableCellElement | null = row.querySelector('td');
-      if (firstTd && firstTd instanceof HTMLTableCellElement) {
+      const firstTd: HTMLDivElement | null = row.querySelector('.tdata');
+      if (firstTd && firstTd instanceof HTMLDivElement) {
         this._makeSticky(firstTd, '0px');
       }
     });
   }
 
-  private _makeSticky(cell: HTMLTableCellElement, left: string): void {
-    this.renderer.setStyle(cell, 'position', 'sticky');
-    this.renderer.setStyle(cell, 'left', left);
-    this.renderer.setStyle(cell, 'z-index', '1');
+  private _makeSticky(cell: HTMLDivElement, left: string): void {
+    // this.renderer.setStyle(cell, 'position', 'sticky');
+    // this.renderer.setStyle(cell, 'left', left);
+    // this.renderer.setStyle(cell, 'z-index', '1');
   }
 
+  private _syncScroll(thead: HTMLDivElement, tbody: HTMLDivElement) {
+    const unlistenThead = this.renderer.listen(thead, 'scroll', () => {
+      tbody.scrollLeft = thead.scrollLeft;
+    });
+
+    const unlistenTbody = this.renderer.listen(tbody, 'scroll', () => {
+      thead.scrollLeft = tbody.scrollLeft;
+    });
+
+    this._unlisteners.push(unlistenThead, unlistenTbody);
+  }
 }
