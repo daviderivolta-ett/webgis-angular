c40b2695693308dc625f3d389da55a846352905a
daviderivolta-ett <davide.rivolta@dedagroup.it>
2026-01-28 11:03:26 +0100

    Removed unused decimal pipe in map popup component

--- src/app/pages/data/map-popup/map-popup.component.ts
+++ src/app/pages/data/map-popup/map-popup.component.ts
@@ -1,6 +1,6 @@
 /** Libraries */
 import { Component, input, output } from '@angular/core'
-import { DatePipe, DecimalPipe } from '@angular/common'
+import { DatePipe } from '@angular/common'
 
 /** Models */
 import { createDefaultStationsPopupConfig, Station, StationPopupConfig } from '../../../models'
@@ -9,8 +9,7 @@ import { createDefaultStationsPopupConfig, Station, StationPopupConfig } from '.
 @Component({
   selector: 'app-map-popup',
   imports: [
-    DatePipe,
-    DecimalPipe
+    DatePipe
   ],
   templateUrl: './map-popup.component.html',
   styleUrl: './map-popup.component.scss'
