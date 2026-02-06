3c6b661fd3490bbc2da0c9064145906360508d10
daviderivolta-ett <davide.rivolta@dedagroup.it>
2026-02-06 12:21:22 +0100

    Tables labels

--- src/app/pages/data/data-page/data-page.component.ts
+++ src/app/pages/data/data-page/data-page.component.ts
@@ -471,6 +471,10 @@ export class DataPageComponent {
   public async onChartParameterChange(stationCode: string, chartId: string, formChange: Record<string, string>): Promise<void> {
     const { param, initialDate, endingDate } = formChange;
 
+    console.log(this.chartReferenceDate);
+    console.log(initialDate);
+    
+
     const chart = this.charts.find((c: MapChart) => c.id === chartId);
     if (!chart) return;
 
