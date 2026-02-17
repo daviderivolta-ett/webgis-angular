e21b71ca34f6e7e2ef310e74ff2ed4c62bfd34ab
daviderivolta-ett <davide.rivolta@dedagroup.it>
2026-02-17 15:41:03 +0100

    Lidar images

--- src/app/models/ui/index.ts
+++ src/app/models/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './chip.class'
 export * from './grouped-checkbox-item.class'
+export * from './lidar-img.interface'
 export * from './lidar.class'
 export * from './map-chart-data.class'
 export * from './map-chart.class'
